6b41fcdd9dc7df8a90a4aa766a29d0662f130287
Bui Van Diep <35633954+vandiepbui@users.noreply.github.com>
2022-03-19 21:20:41 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,53 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
       ),
     );
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text(
+          'Ask me anything',
+          style: TextStyle(fontSize: 25.0),
+        ),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: AnswerPage(),
+    );
+  }
+}
+
+class AnswerPage extends StatefulWidget {
+  @override
+  State<AnswerPage> createState() => _AnswerPageState();
+}
+
+class _AnswerPageState extends State<AnswerPage> {
+  var answerNum = 1;
+  void onChangeAnswer() {
+    setState(() {
+      answerNum = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          onChangeAnswer();
+        },
+        child: Image.asset('images/ball$answerNum.png'),
+      ),
+    );
+  }
+}
